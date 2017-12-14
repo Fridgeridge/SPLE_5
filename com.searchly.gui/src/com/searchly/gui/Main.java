f9714eaa7080ac16b29fe9659ef3aa31e9e539c9
fran_tb <tobias@141.72.113.167>
2017-12-14 15:12:42 +0100

    Add simple base project for own GUI code

---
+++ com.searchly.gui/src/com/searchly/gui/Main.java
@@ -0,0 +1,12 @@
+package com.searchly.gui;
+
+import com.searchly.gui.home.Home;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Home mainFrame = new Home();
+		mainFrame.setVisible(true);
+	}
+
+}
